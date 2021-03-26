d9c617af8ee7be4b4ae11ebeadd5fb62efb6b740
Satyam-001510 <79755634+Satyam-001510@users.noreply.github.com>
2021-03-26 16:58:22 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+    /*var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);*/
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+  }
+}
+
